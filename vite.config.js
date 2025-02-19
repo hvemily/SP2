6ae8d092a9c851132ee0e6ed83eb69a90c9d3251
Emily <126881207+hvemily@users.noreply.github.com>
2025-02-19 13:06:48 +0100

    updating vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/", // Viktig for at Netlify skal finne riktige paths!
+  appType: "mpa", // Multi-page Application
+  base: "", // Tom base for relativ navigasjon
   build: {
-    outDir: "dist",
-  },
-  server: {
-    port: 5173,
-    open: true,
+    sourcemap: true,
+    target: "esnext",
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"), // Hovedsiden
+        login: resolve(__dirname, "auth/login/index.html"),
+        register: resolve(__dirname, "auth/register/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+        category: resolve(__dirname, "listing/category/index.html"),
+        create: resolve(__dirname, "listing/create/index.html"),
+        edit: resolve(__dirname, "listing/edit/index.html"),
+      },
+    },
   },
 });
